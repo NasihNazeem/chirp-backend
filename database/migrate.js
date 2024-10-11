ef7e97096878199766d1ae574c322e542c2d68c2
NasihNazeem <nasihnazeem@gmail.com>
2024-10-10 20:32:19 -0600

    updated undefined env var

--- database/migrate.js
+++ database/migrate.js
@@ -2,7 +2,7 @@ const { Client } = require("pg");
 require("dotenv").config();
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_URL,
 });
 
 const runSQLFiles = async () => {
