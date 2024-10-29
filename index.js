b60091b53a917cce542b39b22543602523690a4a
NasihNazeem <nasihnazeem@gmail.com>
2024-10-28 18:35:30 -0600

    test edit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 const port = process.env.SERVER_PORT || 3001;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://your-frontend-url.com"); // Replace with your frontend's URL
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with your frontend's URL
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true"); // Allow cookies, headers, etc.
