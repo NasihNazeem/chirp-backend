be4fd10f933007c88269825571dff8687597723e
NasihNazeem <nasihnazeem@gmail.com>
2024-10-28 18:31:24 -0600

    testing res.middleware

--- index.js
+++ index.js
@@ -16,10 +16,17 @@ const followRoute = require("./routes/followRoutes");
 const app = express();
 const port = process.env.SERVER_PORT || 3001;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://your-frontend-url.com"); // Replace with your frontend's URL
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true"); // Allow cookies, headers, etc.
+  next();
+});
+
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
     headers: ["Authorization", "Content-Type"],
     credentials: true,
   })
